28d6ea783dd02f62c60d5eac76dd9aed8b511dce
viz-nu <vishnu.teja101.vt@gmail.com>
2025-05-20 18:36:13 +0530

    fix: twilio

--- src/utils/twillio.js
+++ src/utils/twillio.js
@@ -1,5 +1,5 @@
 // Option 1: Using default import (recommended)
-import twilio, { twiml as TwiML } from 'twilio';
+import twilio from 'twilio';
 import { configDotenv } from 'dotenv';
 configDotenv();
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, PHONE_NUMBER_FROM, DOMAIN } = process.env;
@@ -8,7 +8,7 @@ export const makeCallUsingTwilio = async (session) => {
     try {
         const { to, _id } = session;
         // ✅ Construct TwiML using Twilio helper
-        const VoiceResponse = TwiML.VoiceResponse;
+        const VoiceResponse = twilio.twiml.VoiceResponse;
         const response = new VoiceResponse();
         const connect = response.connect();
         connect.stream({ url: `wss://${DOMAIN.replace(/^https?:\/\//, '')}/media-stream?sessionId=${_id}`, });
